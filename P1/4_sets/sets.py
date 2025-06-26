6da3c78e6185c19eb6276dff3e6fc0a13bb89ede
Jesus_Roberto <jesus_3141240176@utd.edu.mx>
2025-06-26 17:58:34 -0600

    Jesus Mendoza
    26/06/2025

--- P1/4_sets/sets.py
+++ P1/4_sets/sets.py
@@ -1,6 +1,4 @@
 """
-
- 
  Sets.- 
   Es un tipo de datos para tener una coleccion de valores pero no tiene ni indice ni orden
 
@@ -10,10 +8,17 @@ import os
 os.system("clear")
 
 
+paises = {"México", "Brazil", "España", "Canada", "Canada"}
+print(paises)
 
+varios =  {True,"Cadena", 23, 3.1416}
+print(varios)
 
+paises.add("Mexico")
+print(paises)
 
-  
+varios.pop()
+print(varios)
 
-
-
+varios.remove("Cadena")
+print(varios)
--- P1/6_tuple/tuple.py
+++ P1/6_tuple/tuple.py
@@ -10,3 +10,17 @@
 """
 import os
 os.system("clear")
+
+
+
+tupla1 = ("apple", "banana", "cherry")
+print(tupla1)
+print(tupla1[0])
+
+tupla2 = ("abc", 34, True, 40, "male")
+print(tupla2)
+
+
+# Metodos de tuplas
+# count()	Returns the number of times a specified value occurs in a tuple
+# index() Searches the tuple for a specified value and returns the position of where it was found
