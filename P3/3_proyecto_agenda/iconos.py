c758bdf7df396002640347d6824a57591efdbe55
Jesus_Roberto <jesus_3141240176@utd.edu.mx>
2025-07-23 23:10:55 -0600

    actualizacion final
    proyectos terminados

---
+++ P3/3_proyecto_agenda/iconos.py
@@ -0,0 +1,34 @@
+# | Categoría         | Emoji | Código Unicode | Uso sugerido                      |
+# | ----------------- | ----- | -------------- | --------------------------------- |
+# | ✅ Confirmación    | ✅     | `\u2705`       | Acción realizada con éxito        |
+# | ❌ Error           | ❌     | `\u274C`       | Error o fallo                     |
+# | ⚠️ Advertencia    | ⚠️    | `\u26A0`       | Avisos o advertencias             |
+# | 📝 Entrada        | 📝    | `\U0001F4DD`   | Indicador de entrada o edición    |
+# | 🔍 Búsqueda       | 🔍    | `\U0001F50D`   | Búsqueda o consulta               |
+# | 📧 Correo         | 📧    | `\U0001F4E7`   | Correo electrónico                |
+# | 📞 Teléfono       | 📞    | `\U0001F4DE`   | Números telefónicos               |
+# | 👤 Usuario        | 👤    | `\U0001F464`   | Identidad de persona/contacto     |
+# | 📂 Archivo        | 📂    | `\U0001F4C2`   | Trabajando con archivos           |
+# | 🕒 Tiempo         | 🕒    | `\U0001F552`   | Para mostrar tiempo o espera      |
+# | 💾 Guardar        | 💾    | `\U0001F4BE`   | Guardar información               |
+# | 🔄 Actualizar     | 🔄    | `\U0001F501`   | Recarga, reinicio, refresco       |
+# | 🚪 Salida         | 🚪    | `\U0001F6AA`   | Para mostrar que se está saliendo |
+# | 📛 Eliminación    | 📛    | `\U0001F4DB`   | Eliminación o cancelación         |
+# | 🎉 Éxito especial | 🎉    | `\U0001F389`   | Felicitaciones o logros           |
+# | 🆕 Nuevo          | 🆕    | `\U0001F195`   | Para indicar algo nuevo           |
+# | 🔒 Seguridad      | 🔒    | `\U0001F512`   | Para indicar bloqueo              |
+# | 🔑 Clave          | 🔑    | `\U0001F511`   | Para indicar acceso o autenticación |
+# | 👉🏽 señalamiento   | 👉🏽    |                | para señalar a la derecha         | 
+
+
+# | Numerios
+# | 1️⃣ Uno            | 1️⃣    | `\U00000031\U000020E3` | Para indicar la opción uno en menús |
+# | 2️⃣ Dos            | 2️⃣    | `\U00000032\U000020E3` | Para indicar la opción dos en menús |
+# | 3️⃣ Tres           | 3️⃣    | `\U00000033\U000  020E3` | Para indicar la opción tres en menús |
+# | 4️⃣ Cuatro         | 4️⃣    | `\U00000034\U000020E3` | Para indicar la opción cuatro en menús |
+# | 5️⃣ Cinco          | 5️⃣    | `\U00000035\U000020E3` | Para indicar la opción cinco en menús |
+# | 6️⃣ Seis           | 6️⃣    | `\U00000036\U000020E3` | Para indicar la opción seis en menús |
+# | 7️⃣ Siete          | 7️⃣    | `\U00000037\U000020E3` | Para indicar la opción siete en menús |
+# | 8️⃣ Ocho           | 8️⃣    | `\U00000038\U000020E3` | Para indicar la opción ocho en menús |
+# | 9️⃣ Nueve          | 9️⃣    | `\U00000039\U000020E3` | Para indicar la opción nueve en menús |
+# | 0️⃣ Cero           | 0️⃣    | `\U00000030\U000020E3` | Para indicar la opción cero en menús |
